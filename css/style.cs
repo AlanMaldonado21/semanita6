f0a1e334aac6774d8ae1161560b61fcd519bfd02
Alan Ivan <maldonadoalanivan@gmail.com>
2024-10-28 22:31:48 -0300

    terminamos la clase 08

---
+++ css/style.cs
@@ -0,0 +1,129 @@
+.comunicado {
+  padding: 10px;
+  background-color: #3DC28F;
+}
+.comunicado p {
+  text-align: center;
+  font-family: Arial, Helvetica, sans-serif;
+  font-size: 13px;
+}
+
+.botones {
+  display: flex;
+  justify-content: space-around;
+  align-items: center;
+}
+.botones .boton-mixin {
+  color: red;
+  background-color: blue;
+  font-family: Helvetica;
+  padding: 50px;
+  text-decoration: underline;
+  border-radius: 5px;
+}
+.botones a:nth-child(2) {
+  background-color: yellow;
+  color: black;
+  text-decoration: none;
+  padding: 15px 10px;
+  border-radius: 5px;
+  font-family: Arial, Helvetica, sans-serif;
+}
+.botones .boton {
+  background-color: yellow;
+  color: black;
+  text-decoration: none;
+  padding: 15px 10px;
+  border-radius: 5px;
+  font-family: Arial, Helvetica, sans-serif;
+}
+
+header {
+  display: flex;
+  justify-content: space-around;
+  align-items: center;
+}
+header img {
+  width: 5rem;
+}
+header nav ul {
+  display: flex;
+  list-style: none;
+}
+header nav ul li {
+  margin: 5px;
+}
+header nav ul li a {
+  color: black;
+  text-decoration: none;
+  font-family: Arial, Helvetica, sans-serif;
+  font-size: 15px;
+}
+header .crear-cuenta {
+  display: flex;
+  align-items: center;
+}
+header .crear-cuenta .ingreso {
+  display: flex;
+  align-items: center;
+}
+header .crear-cuenta .ingreso a {
+  text-decoration: none;
+  color: red;
+  font-family: Arial;
+  font-size: 14px;
+}
+header .crear-cuenta .ingreso img {
+  width: 35px;
+}
+header .crear-cuenta a:last-child {
+  margin-left: 15px;
+  color: white;
+  text-decoration: none;
+  font-family: Arial;
+  background-color: blue;
+  font-size: 14px;
+  padding: 10px;
+  border-radius: 5px;
+}
+
+* {
+  margin: 0;
+  padding: 0;
+}
+
+body {
+  background-color: #F2F7FD;
+}
+
+.botones {
+  display: flex;
+  justify-content: space-around;
+  align-items: center;
+}
+.botones .boton-mixin {
+  color: red;
+  background-color: blue;
+  font-family: Helvetica;
+  padding: 50px;
+  text-decoration: underline;
+  border-radius: 5px;
+}
+.botones a:nth-child(2) {
+  background-color: yellow;
+  color: black;
+  text-decoration: none;
+  padding: 15px 10px;
+  border-radius: 5px;
+  font-family: Arial, Helvetica, sans-serif;
+}
+.botones .boton {
+  background-color: yellow;
+  color: black;
+  text-decoration: none;
+  padding: 15px 10px;
+  border-radius: 5px;
+  font-family: Arial, Helvetica, sans-serif;
+}
+
+/*# sourceMappingURL=style.cs.map */
